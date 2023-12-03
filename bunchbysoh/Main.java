897f89196ad6a8d0c36ce6c77b74f98063593f7f
Akashjoshi2272 <117998691+Akashjoshi2272@users.noreply.github.com>
2023-12-03 17:36:32 +0530

    Update Main.java

--- bunchbysoh/Main.java
+++ bunchbysoh/Main.java
@@ -9,6 +9,17 @@ public class Main {
 
   static CountsBySoH countBatteriesByHealth(int[] presentCapacities) {
     CountsBySoH counts = new CountsBySoH();
+    for (int capacity : presentCapacities) {
+            double SoH = 100.0 * capacity / 120;
+
+            if (SoH > 80) {
+                counts.healthy++;
+            } else if (SoH <= 80 && SoH >= 62) {
+                counts.exchange++;
+            } else {
+                counts.failed++;
+            }
+        }
     return counts;
   }
 
@@ -19,6 +30,40 @@ public class Main {
     assert(counts.healthy == 2);
     assert(counts.exchange == 3);
     assert(counts.failed == 1);
+
+    // Lowest capacity (boundary condition)
+    int[] lowestCapacity = {0};
+    CountsBySoH lowestCounts = countBatteriesByHealth(lowestCapacity);
+    assert(lowestCounts.failed == 1);
+    
+    // Highest capacity (boundary condition)
+    int[] highestCapacity = {120};
+    CountsBySoH highestCounts = countBatteriesByHealth(highestCapacity);
+    assert(highestCounts.healthy == 1);
+    
+    // Equal to boundary values (boundary condition)
+    int[] boundaryValues = {96, 80, 74};
+    CountsBySoH boundaryCounts = countBatteriesByHealth(boundaryValues);
+    assert(boundaryCounts.healthy == 1);
+    assert(boundaryCounts.exchange == 1);
+    assert(boundaryCounts.failed == 1);
+    
+    // Single battery (boundary condition)
+    int[] singleBattery = {70};
+    CountsBySoH singleCounts = countBatteriesByHealth(singleBattery);
+    assert(singleCounts.failed == 1);
+    
+    // Empty battery list (boundary condition)
+    int[] emptyList = {};
+    CountsBySoH emptyCounts = countBatteriesByHealth(emptyList);
+    assert(emptyCounts.healthy == 0 && emptyCounts.exchange == 0 && emptyCounts.failed == 0);
+    
+    // Large dataset (boundary condition)
+    int[] largeDataSet = {110, 115, 80, 95, 92, 70, 105, 98, 75, 88, 77, 120, 62, 45};
+    CountsBySoH largeDataCounts = countBatteriesByHealth(largeDataSet);
+    assert(largeDataCounts.healthy == 6);
+    assert(largeDataCounts.exchange == 4);
+    assert(largeDataCounts.failed == 4);
     System.out.println("Done counting :)\n");
   }
 
